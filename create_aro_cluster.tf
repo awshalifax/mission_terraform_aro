e9009c3fdaab0ed4fec459191e038df7bd13f81e
William <awshalifax@gmail.com>
2021-11-08 16:35:22 -0400

    destroy cluster working, moved variables to variables.tf

--- create_aro_cluster.tf
+++ create_aro_cluster.tf
@@ -2,34 +2,65 @@ variable "deploy" {
   description = "true = Create Cluster | false = Destroy cluster"
 }
 
-locals { #testar sem isso aqui, colocar direto nos comandos
-  ms     = "master-sub"
-  wk     = "worker-sub"
-  rg     = azurerm_resource_group.openshift-cluster.name
-  loc    = var.location
-  vnet   = module.arovnet.vnet_name
-  name   = "aro01"
-  domain = azurerm_dns_zone.aro-dns-zone.name
+locals {
+  ms        = var.master_subnet_name
+  ms-cidr   = var.master_cidr
+  wk        = var.worker_subnet_name
+  wk-cidr   = var.worker_cidr
+  rg        = azurerm_resource_group.openshift-cluster.name
+  loc       = var.location
+  vnet      = var.vnet_name
+  vnet-cidr = var.vnet_cidr
+  name      = var.openshift_cluster_name
+  domain    = azurerm_dns_zone.aro-dns-zone.name
 }
 
-#az aro create -g openshift-cluster-dev -n aro01 --vnet virtual-network-dev --master-subnet master-subnet --worker-subnet worker-subnet --domain williamopenshift.eastus.aroapp.io --location eastus --pull-secret @pull-secret
-resource "null_resource" "create-cluster" {
+resource "null_resource" "create-vnet" {
+  count = var.deploy ? 1 : 0
+  provisioner "local-exec" {
+    command = "az network vnet create --resource-group ${local.rg} --name ${local.vnet} --address-prefixes ${local.vnet-cidr}"
+  }
+
+  depends_on = [
+    azurerm_dns_zone.aro-dns-zone,
+    azurerm_resource_group.openshift-cluster
+  ]
+}
+
+resource "null_resource" "create-master-subnet" {
+  count = var.deploy ? 1 : 0
+  provisioner "local-exec" {
+    command = "az network vnet subnet create --resource-group ${local.rg} --vnet-name ${local.vnet} --name ${local.ms} --address-prefixes ${local.ms-cidr} --service-endpoints Microsoft.ContainerRegistry --disable-private-link-service-network-policies true"
+  }
+
+  depends_on = [
+    null_resource.create-vnet
+  ]
+}
+
+resource "null_resource" "create-worker-subnet" {
+  count = var.deploy ? 1 : 0
+  provisioner "local-exec" {
+    command = "az network vnet subnet create --resource-group ${local.rg} --vnet-name ${local.vnet} --name ${local.wk} --address-prefixes ${local.wk-cidr} --service-endpoints Microsoft.ContainerRegistry --disable-private-link-service-network-policies true"
+  }
+
+  depends_on = [
+    null_resource.create-master-subnet
+  ]
+}
+
+resource "null_resource" "create-aro-cluster" {
   count = var.deploy ? 1 : 0
   provisioner "local-exec" {
     command = "az aro create -g ${local.rg} -n ${local.name} --vnet ${local.vnet} --master-subnet ${local.ms} --worker-subnet ${local.wk} --domain ${local.domain} --location ${local.loc} --pull-secret @pull-secret"
   }
 
   depends_on = [
-    azurerm_dns_zone.aro-dns-zone,
-    azurerm_resource_group.openshift-cluster,
-    module.arovnet
-
+    null_resource.create-worker-subnet
   ]
-
 }
 
-#az aro delete -n aro01 -g openshift-cluster-dev --yes
-resource "null_resource" "delete" {
+resource "null_resource" "delete-aro-cluster" {
   count = var.deploy ? 0 : 1
   provisioner "local-exec" {
     command = "az aro delete -n ${local.name} -g ${local.rg} --yes"
--- variables.tf
+++ variables.tf
@@ -1,5 +1,3 @@
-# Input variables are parameters for Terraform modules
-
 variable "workspace_to_environment_map" {
   type = map
   default = {
@@ -13,4 +11,46 @@ variable "location" {
   description = "The location where the resource group should be created"
   type        = string
   default     = "eastus"
+}
+
+variable "vnet_cidr" {
+  description = "The location where the resource group should be created"
+  type        = string
+  default     = "10.0.0.0/22"
+}
+
+variable "master_cidr" {
+  description = "The location where the resource group should be created"
+  type        = string
+  default     = "10.0.0.0/23"
+}
+
+variable "worker_cidr" {
+  description = "The location where the resource group should be created"
+  type        = string
+  default     = "10.0.2.0/23"
+}
+
+variable "vnet_name" {
+  description = "The location where the resource group should be created"
+  type        = string
+  default     = "openshift-aro-vnet"
+}
+
+variable "master_subnet_name" {
+  description = "The location where the resource group should be created"
+  type        = string
+  default     = "master-subnet"
+}
+
+variable "worker_subnet_name" {
+  description = "The location where the resource group should be created"
+  type        = string
+  default     = "worker-subnet"
+}
+
+variable "openshift_cluster_name" {
+  description = "The location where the resource group should be created"
+  type        = string
+  default     = "openshift-cluster"
 }
