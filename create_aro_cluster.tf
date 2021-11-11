5badd77034574e4ddbff68efaa4259fa9502c636
William <awshalifax@gmail.com>
2021-11-11 15:39:16 -0400

    final commit terraform

--- create_aro_cluster.tf
+++ create_aro_cluster.tf
@@ -1,22 +1,19 @@
-variable "deploy" {
-  description = "true = Create cluster | false = Destroy cluster"
-}
-
 locals {
-  ms        = var.master_subnet_name
-  ms-cidr   = var.master_cidr
-  wk        = var.worker_subnet_name
-  wk-cidr   = var.worker_cidr
-  rg        = azurerm_resource_group.openshift-cluster.name
-  loc       = var.location
-  vnet      = var.vnet_name
-  vnet-cidr = var.vnet_cidr
-  name      = var.openshift_cluster_name
-  domain    = azurerm_dns_zone.aro-dns-zone.name
+  ms            = var.master_subnet_name
+  ms-cidr       = var.master_cidr
+  wk            = var.worker_subnet_name
+  wk-cidr       = var.worker_cidr
+  rg            = azurerm_resource_group.openshift-cluster.name
+  loc           = var.location
+  vnet          = var.vnet_name
+  vnet-cidr     = var.vnet_cidr
+  name          = var.openshift_cluster_name
+  domain        = azurerm_dns_zone.aro-dns-zone.name
+  client_id     = "6766b56e-ff03-4299-9d08-a8d4754d62c8"
+  client_secret = "G.mh~kHYdRAooOEYIgnSUF4rxqkVG9H0F3"
 }
 
 resource "null_resource" "create-vnet" {
-  count = var.deploy ? 1 : 0
   provisioner "local-exec" {
     command = "az network vnet create --resource-group ${local.rg} --name ${local.vnet} --address-prefixes ${local.vnet-cidr}"
   }
@@ -28,7 +25,6 @@ resource "null_resource" "create-vnet" {
 }
 
 resource "null_resource" "create-master-subnet" {
-  count = var.deploy ? 1 : 0
   provisioner "local-exec" {
     command = "az network vnet subnet create --resource-group ${local.rg} --vnet-name ${local.vnet} --name ${local.ms} --address-prefixes ${local.ms-cidr} --service-endpoints Microsoft.ContainerRegistry --disable-private-link-service-network-policies true"
   }
@@ -39,7 +35,6 @@ resource "null_resource" "create-master-subnet" {
 }
 
 resource "null_resource" "create-worker-subnet" {
-  count = var.deploy ? 1 : 0
   provisioner "local-exec" {
     command = "az network vnet subnet create --resource-group ${local.rg} --vnet-name ${local.vnet} --name ${local.wk} --address-prefixes ${local.wk-cidr} --service-endpoints Microsoft.ContainerRegistry --disable-private-link-service-network-policies true"
   }
@@ -50,22 +45,11 @@ resource "null_resource" "create-worker-subnet" {
 }
 
 resource "null_resource" "create-aro-cluster" {
-  count = var.deploy ? 1 : 0
   provisioner "local-exec" {
-    command = "az aro create -g ${local.rg} -n ${local.name} --vnet ${local.vnet} --master-subnet ${local.ms} --worker-subnet ${local.wk} --domain ${local.domain} --location ${local.loc} --pull-secret @pull-secret"
+    command = "az aro create --client-id ${local.client_id} --client-secret ${local.client_secret} -g ${local.rg} -n ${local.name} --vnet ${local.vnet} --master-subnet ${local.ms} --worker-subnet ${local.wk} --domain ${local.domain} --location ${local.loc} --pull-secret @pull-secret"
   }
 
   depends_on = [
     null_resource.create-worker-subnet
   ]
 }
-
-resource "null_resource" "delete-aro-cluster" {
-  count = var.deploy ? 0 : 1
-  provisioner "local-exec" {
-    command = "az aro delete -n ${local.name} -g ${local.rg} --yes"
-  }
-}
-
-
-
